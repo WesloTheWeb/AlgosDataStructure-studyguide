8a5835b5925c9532a9d2babd9b00816a66efe4b0
Wesley Webster <wesleyadwebster@gmail.com>
2022-01-16 23:05:55 -0600

    added in another solution to insertion loop that uses while

---
+++ Algorithms/Sorting/insertionSort_reference-2.js
@@ -0,0 +1,18 @@
+const arr = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+
+function insertionSort(array) {
+    const length = array.length;
+
+    for (let i = 1; i < length; i++) {
+        let key = array[i];
+        let j = i - 1;
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+    return array;
+};
+
+console.log(insertionSort(arr));
