5220a9b2f0a6b0e6f4a29f4aebcb5b4848561571
Wesley <wesleyadwebster@gmail.com>
2022-01-16 19:50:05 -0600

    Added in a solution via insertion sort file to use as reference

---
+++ Algorithms/Sorting/insertionSort_solution.js
@@ -0,0 +1,25 @@
+const arr = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+
+function insertionSort(array) {
+    const length = array.length;
+
+    for (let i = 1; i < length; i++) {
+
+        for (let j = i; j > 0; j--) {
+            if (array[j] < array[j - 1]) {
+                // const temp = array[j];
+                // array[j] = array[j-1];
+                // array[j-1] = temp;
+
+                // simplified way:
+                [array[j], array[j - 1]] = [array[j - 1], array[j]]
+            } else {
+                break;
+            }
+        };
+    };
+
+    return array;
+};
+
+console.log(insertionSort(arr));
