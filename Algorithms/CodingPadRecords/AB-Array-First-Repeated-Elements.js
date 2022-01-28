ebea36be2ae4b5118678b17e779845e5b2e5eff5
Wesley Webster <wesleyadwebster@gmail.com>
2022-01-27 22:07:23 -0600

    repeated elements

---
+++ Algorithms/CodingPadRecords/AB-Array-First-Repeated-Elements.js
@@ -0,0 +1,24 @@
+/* Q. Given an array of positive integers, find the first element that occurs k number of times. 
+// If no element occurs k times, return -1, and you may assume k is greater than 0. 
+// Examples:
+// • Given an array: [1, 2, 2, 3, 3], k: 2 // returns 2
+// • Given an array: [], k: 1 // returns -1
+*/
+
+// O(N) time
+function firstKTimes(array, k) {
+    const counts = new Map();
+
+    for (const el of array) {
+        const count = (counts.get(el) || 0) + 1;
+        if (count === k) return el;
+        counts.set(el, count);
+    }
+}
+
+// Test Cases
+console.log(firstKTimes([1, 2, 2, 3, 3], 2)) // 2
+console.log(firstKTimes([1, 2, 2, 3, 3], 3)) // -1
+console.log(firstKTimes([], 1)) // -1
+
+// NOTE TO SELF: Study Dictionaries JS
---
+++ Algorithms/CodingPadRecords/AB-BinaryTree-Find-Max-Recursive.js
@@ -0,0 +1,50 @@
+/* 
+Q. Given a binary tree, find the element with the largest value.
+
+Example:
+• Given a binary tree:
+                 1
+                / \
+               7   3
+              / \
+             4   5
+// returns 7 
+*/
+
+class TreeNode {
+    constructor(value = 0, leftChild = null, rightChild = null) {
+        this.value = value
+        this.left = leftChild
+        this.right = rightChild
+    }
+}
+
+// function findTreeMax(root) {
+//     let maxRootValue = root.value;
+
+//     if (root == null) {
+//         return null;
+//     }
+//     if (root.left) {
+//         maxRootValue = Math.max(maxRootValue, findTreeMax(root.left));
+//     }
+//     if (root.right) {
+//         maxRootValue = Math.max(maxRootValue, findTreeMax(root.right));
+//     }
+//     return maxRootValue;
+// }
+
+function findTreeMax(root) {   
+    if (!root) {
+      return null
+    }
+    
+    return Math.max(root.value, 
+                    findTreeMax(root.left) || -Infinity,
+                    findTreeMax(root.right) || -Infinity)
+  }
+// Test Cases
+console.log(findTreeMax(null)) // null
+console.log(findTreeMax(new TreeNode(1, new TreeNode(2), new TreeNode(3)))) // 3
+console.log(findTreeMax(new TreeNode(2, new TreeNode(29, new TreeNode(26)), new TreeNode(4, null, new TreeNode(2, new TreeNode(9)))))) // 29
+console.log(findTreeMax(new TreeNode(1))) // 1
