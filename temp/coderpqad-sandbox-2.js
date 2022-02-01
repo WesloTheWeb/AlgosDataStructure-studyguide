477005912a2d8b00c7b3af6db60d82e3925dd02e
Wesley Webster <wesleyadwebster@gmail.com>
2022-02-01 12:23:00 -0600

    Selection sort added

---
+++ temp/coderpqad-sandbox-2.js
@@ -0,0 +1,129 @@
+/*##################################################################################
+TAG: Validate a Binary Search Tree - 4m
+#####################################################################################
+
+Q. Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Examples:
+• Given a binary tree:
+        2
+       / \
+      1   3
+returns True
+
+• Given a binary tree:
+        1
+       / \
+      2   3
+returns False
+*/
+
+class TreeNode {
+    constructor(value = 0, leftChild = null, rightChild = null) {
+        this.value = value
+        this.left = leftChild
+        this.right = rightChild
+    }
+}
+
+function validateBST(node, min = -Infinity, max = Infinity) {
+    if (!node) {
+        return true;
+    } else if (node.value <= min || node.value > max) {
+        return false;
+    }
+
+    return (
+        validateBST(node.left, min, node.value) &&
+        validateBST(node.right, node.value, max)
+    );
+}
+// Test Cases
+var tree1 = new TreeNode(2, new TreeNode(1), new TreeNode(3))
+var tree2 = new TreeNode(1, new TreeNode(2), new TreeNode(3))
+var tree3 = new TreeNode(8, new TreeNode(3, new TreeNode(1), new TreeNode(6)), new TreeNode(10, null, new TreeNode(14, new TreeNode(13))))
+console.log(validateBST(null)) // true
+console.log(validateBST(tree1)) // true
+console.log(validateBST(tree2)) // false
+console.log(validateBST(tree3)) // true
+console.log(validateBST(new TreeNode())) // true
+
+/////
+/*##########################################################################################
+SOURCE: Linked List - Inserting an Element.
+Question: Given a sorted linked list, insert an element in the appropriate position. This is following a sorted order 
+linked list.
+############################################################################################*/
+
+/*############################
+Iterative Solution
+############################*/
+// O(N) time
+
+/*
+
+     H
+1 -> 2 -> 3 -> 4 -> 5-> 6 -> 7               
+
+               
+               
+
+
+
+
+// INSERT 5 IN LINKEDLIST
+
+let curr = head;   // let i
+
+while (curr) { // equivalent of the middle section in a for loop 
+  // do something
+  curr = curr.next.next // equivalent of  in for loop
+}
+
+
+*/
+
+// function insert(head, target) {
+//     if (!head) return new ListNode(target);
+
+//     let dummyHead = new ListNode(0);
+//     dummyHead.next = head;
+//     let node = dummyHead;
+
+//     // I need know whats exactly happen with the logic in line 21 - 27 while the node is true.
+//     while (node) {
+//         if (!node.next || target < node.next.value) {
+//             const next = node.next;
+//             node.next = new ListNode(target);
+//             node.next.next = next;
+//             break;
+//         }
+//         node = node.next;
+//     }
+
+//     return dummyHead.next;
+// };
+
+// /*############################
+// Recursive Solution
+// ############################*/
+
+// function insert(head, target) {
+//     let node = head;
+
+//     // Likewise, I don't know how to read "new ListNode(target, node)" line 43 and 45. 
+//     if (!node) { //line 109->113 same as while loop condition
+//         return new ListNode(target);
+//     } else if (target < node.value) {
+//         return new ListNode(target, node);
+//     } else {
+//         node.next = insert(node.next, target); // ++i
+//     }
+
+//     return head;
+// }
+
+/*##########################################################################################
+SOURCE: Linked List - Inserting an Element.
+Question: Given a linked list, append an element with a target value to the list iteratively.
+############################################################################################*/
