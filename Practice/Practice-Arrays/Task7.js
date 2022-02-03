4154a2d52659598117e1f36c4690cbd975ae620e
Wesley <wesleyadwebster@gmail.com>
2022-02-03 03:12:14 -0600

    added Linked List and Array problems to practice and solve in future

---
+++ Practice/Practice-Arrays/Task7.js
@@ -0,0 +1,8 @@
+/*
+Q. Given an array of integers in ascending order, return a array of the squares of each number sorted in ascending order.
+
+*/
+
+function solution(array) {
+    // write code
+};
---
+++ Practice/Practice-Arrays/Task8.js
@@ -0,0 +1,6 @@
+/*
+Q. Given an array of integers, reverse the array by blocks of k.
+*/
+function solution(array, k) {
+
+}
