7a150774ceaeb14d9ca33c12f15e500c67a59a77
Wesley Webster <wesleyadwebster@gmail.com>
2022-02-06 13:30:51 -0600

    Binary tree problems added with Fibonacci walkthrough

---
+++ Fibonacci-sprint.js
@@ -0,0 +1,126 @@
+// Saturday Feb 6
+/*0a. Code the original fibonacci sequence with an array.
+0b. Code the original fibonacci sequence with two variables that are replaced as you go. (go over space complexity differences)
+Return the kth number of an alternative sequence that starts with 1, 1, 1 and each subsequent number is the sum of the previous THREE elements. (1, 1, 1, 3, 5, 9, 17...)
+Even Fibonacci Sequence. In this sequence, round each element to an even number without effecting the overall sequence.
+2, 2, 2, 4, 6, 8, 14, 22...
+Notice that the first two elements get rounded to 2, but the following element is based on the original value, not the rounded values.
+Delayed Fibonacci Sequence. This sequence starts with 1, 1, 1, and then each subsequent number is the sum of the element 2 before and 3 before it (skipping the element right before it).
+1, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 16...
+*/
+
+const _ = require('lodash');
+
+function sayHello() {
+  console.log('Hello, World');
+}
+
+_.times(5, sayHello);
+
+
+/* 
+Your previous Python 3 content is preserved below:
+
+
+# 
+# Your previous JavaScript content is preserved below:
+# 
+# /*
+# 0a. Code the original fibonacci sequence with an array. 
+# 
+# [0, 1, 1, 2]
+# 
+# fib(6) -> [n , n, n, n, n]
+#           [0, 1,  1, 2, 3, 5]
+#    fib(n -1) + fib(n - 2) = fib(n)
+# 
+# 1. Iterative
+# 2. Recursive
+# */
+# 
+# function fib(n) {
+#   let results = [0,1]
+#   // let prev = 1;
+#   // let cur = 1;
+#   
+#   for (let i = 2; i < n; i++) {
+#        let sum = results[i - 2] + results[i - 1] //
+#        results[i] = sum
+#   }
+#   
+#   return results;
+# }
+# console.log(fib(5)) // [0, 1,  1, 2, 3];
+# console.log(fib(3)) //
+# console.log(fib(10)) //
+
+
+
+'''
+0b. Code the original fibonacci sequence with three variables that are replaced as you go. 
+(go over space complexity differences)
+
+
+// Taking no more space that we start with
+// let a, b, c 
+
+
+fib(5)
+n-2 = 1 
+n-1 = 2
+n= 3
+'''
+'''
+def fib(n):
+    n2=0
+    n1=1
+    print(n2, n1)
+    for i in range(2,n):
+        current=n2+n1
+        print(current)
+        n2=n1
+        n1=current
+
+## fib(5) ## [0, 1,  1, 2, 3];
+fib(10)
+'''
+
+'''
+Return the kth number of an alternative sequence that starts with 1, 1, 1 and each subsequent number is the sum of the previous THREE elements. (1, 1, 1, 3, 5, 9, 17...)
+
+k = 1 [1,1,1] -> 1 (first element)
+k = 4 [1,1,1,3,5,9] -> 3
+
+[1,1,1]
+- Three variables
+n1 = 1
+n2 = 1
+n3 = 1
+
+sumOfPreviousThreeNums = n1 + n2 + n3
+print(sumOfPreviousThreeNums) // 3
+n1 = n2   // 1
+n2 = n3   // 1
+n3 = sumOfPreviousThreeNums // 3
+
+k = 7
+
+'''
+
+def kthElementOfAlternativeSequence(k):
+    n1=1
+    n2=1
+    n3=1
+    if k<4:
+        return 1
+    for i in range(3,k):
+        sumOfPreviousThreeNums=n1+n2+n3
+        n1=n2
+        n2=n3
+        n3=sumOfPreviousThreeNums
+    return sumOfPreviousThreeNums
+
+print(kthElementOfAlternativeSequence(3)) ## [1,1,1] -> 1
+print(kthElementOfAlternativeSequence(4)) ## [1,1,1, 3, 5] -> 3
+print(kthElementOfAlternativeSequence(7)) ## 1, 1, 1, 3, 5, 9, 17 -> 17
+ */
---
+++ Practice/BinaryTrees/Task18.js
@@ -0,0 +1,25 @@
+/*
+
+Q. Given a binary search tree, return the value of the violating node. When there is a violation, return the lowest node. If there is no violating node, return -1.
+
+For example, in this tree:
+
+      5
+    /  \
+  2    10
+   \
+    8
+We have a violation between 5 and 8 because 8 is not less than 5. Because 8 is the lower node, return 8.
+
+Binary trees are already defined with this interface:
+function Tree(x) {
+  this.value = x;
+  this.left = null;
+  this.right = null;
+}
+
+*/
+
+function solution(root) {
+
+}
