d701080766b9295300a14a0cdf6c891297821d02
Wesley <wesleyadwebster@gmail.com>
2022-02-13 15:44:21 -0600

    edit to a sprint

--- Algo-sprint-tree.js
+++ Algo-sprint-tree.js
@@ -1,44 +1,40 @@
-'''
-Finding the height of a tree
+/* Finding the height of a tree
 
-1. Find of tree using recursion (basic)
-2. Find height of tree using BFS (queue)
-3. Find height of tree using DFS (stack)
+1. Find of tree using recursion(basic)
+2. Find height of tree using BFS(queue)
+3. Find height of tree using DFS(stack)
 
 class Node:
-    List<Node> children;
+List < Node > children;
     int val;
 
-         3
-       / | \
-      4  5  6 
-     / //\\
-     1 1234
+      3
+    / | \
+    4  5  6
+    / //\\
+    1 1234
 
 
 JEREMY's EXAMPLE
 Finding a Node that has a value in a tree
 
-         3
-       / | \
-      4  5  6 
-     / //\\
-     1 1234
-     ^
-find(root, 1)
+            3
+       /    |     \
+      4     5     6
+      /  / / \ \
+     1  1 2  3  4
+        ^
+        find(root, 1)
 
 base case:
 if null -> return null
 
 recursive case:
 for every child of root.children:
-    foundNode = helperMethod(child)
-    if foundNode != null -> return foundNode
+foundNode = helperMethod(child)
+if foundNode != null -> return foundNode
 
-
-
-
-1. Find of tree using recursion (basic)
+1. Find of tree using recursion(basic)
 
 CONTRACT
 
@@ -48,77 +44,76 @@ if null -> 0
 recursive case:
 maxHeight = 0
 for every child of root.children:
+height = heightOfTree(child)
+if height > maxHeight:
+    maxHeight = height
+return maxHeight
+
+function heightOfTree(root) {
+      base case:
+    if (!root) = 0;
+
+  recursive case:
+    let maxHeight = 0
+
+    for every child of root.children:
     height = heightOfTree(child)
     if height > maxHeight:
         maxHeight = height
-return maxHeight    
-
-'''
-
-function heightOfTree(root) {
-#     base case:
-    if (!root) = 0;
-
-# recursive case:
-    let maxHeight = 0
-    
-# for every child of root.children:
-#     height = heightOfTree(child)
-#     if height > maxHeight:
-#         maxHeight = height
     let children = root.children; // Array<Node>
-    for (let i = 0; i < root.children.length; i++ ) {
+    for (let i = 0; i < root.children.length; i++) {
         let child = children[i];
-        let height = heightOfTree(child) + 1; 
+        let height = heightOfTree(child) + 1;
         if height > maxHeight;
         maxHeight = height;
     }
-    
-    # return maxHeight    
+
+    return maxHeight
     return maxHeight;
 }
 
-'''
-         3
-       / | \
-      4  5  6 
-         |
-         1
+3
+    / | \
+4  5  6
+    |
+    1
 
 1
-|
-2
-|
-3
-|
-3
-|
-3
-|
-3
-|
-3
-|
-3
-
+    |
+    2
+    |
+    3
+    |
+    3
+    |
+    3
+    |
+    3
+    |
+    3
+    |
+    3
 
 Call Stack:
+*/
+
+// heightOfTree(3); maxHeight = 2
+// Time: O(n)
+// Space: O(n)
 
 
-heightOfTree(3); maxHeight = 2
-Time: O(n)
-Space: O(n)
-'''
+console.log(heightOfTree())
 
-print(heightOfTree())
-# function heightOfTree(root) {
-#     if (!root) return -1;
-#     maxChild = root
-#     height = 0
-#     while maxChild.children:
-#         height += 1
-#     return heightOfTree(maxChild)
-#         return 1 + Math.max(heightOfTree(children))
-#     
-#     return 1 + Math.max(heightOfTree(root.left), heightOfTree(root.right))
-# // }
+const heightOfTree = (root) => {
+    if (!root) return -1;
+
+    let maxChild = root;
+    height = 0;
+
+    while (maxChild) {
+        height += 1;
+        return heightOfTree(maxChild);
+        //    return 1 + Math.max(heightOfTree(children))
+        //    return 1 + Math.max(heightOfTree(root.left), heightOfTree(root.right))
+    };
+};
