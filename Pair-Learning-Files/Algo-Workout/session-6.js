c7f429848c2b79934eac5eb5a497b9b8b338f6f5
Wesley Webster <wesleyadwebster@gmail.com>
2022-02-18 16:04:05 -0600

    workout problem added

--- Pair-Learning-Files/Algo-Workout/session-6.js
+++ Pair-Learning-Files/Algo-Workout/session-6.js
@@ -1,80 +1,130 @@
-/*You are given a series of inputs representing delivery events. Each event is represented by an array of length 3:
-[1, 1591846068, 0]
-
-    The first element is an order number
-    The second is the timestamp
-    The third is either 0 (representing a pickup) or 1 (representing a dropoff)
-
-Given a series of events, return the total active time, calculated by the period of time where they have an active delivery (if they've dropped everything off, they are not considered active until they pick something up again).
-                                                                                                                             
-Input:
-[1, 1591846068, 0] numOrders: 0 -> 1 | startTime = 1591846068
-[2, 1591846070, 0] numOrders: 1 -> 2 | startTime = 1591846068
-[1, 1591846071, 1] numOrder: 2 -> 1 | startTime = 1591846068
-[2, 1591846080, 1] numOrder: 1 -> 0 | currTime = 1591846080 | currTime-startTime = 12
-[3, 1591846090, 0] numOrder: 0 -> 1 | startTime = 1591846090
-[3, 1591846102, 1] numOrder: 1-> 0 | currTime = 1591846102 | currTime-startTime = 12
-Output: 24
+can someone post that back
 
 
-Function Signature
-func activeDeliveryTime(events: [Int]) -> Int
+/*
 
-numOrders - number of orders
-startTime
-totalTime
+You are given a binary tree that is represented by a string. Nodes have no value. A 0 represents null and a (00) represents a leaf node. ((00)0) would represent a tree where the root has one left node and no right node. ((00)(00)) represents a tree with one left and one right node. 
 
-loop through the events
-  1. increase/decrease numOrders based on event type (pickup / drop off)
-  2. If we have 1 order, and the event was a pickup
-    a. Save startTime (ie. We became active)
-  3. If we have 0 order now, that means we became inactive
-    a. subtract current time - startTime & add to totalTime
-return totalTime calculated
+---------
+
+((00)0)
+
+   n
+  /   -> 2
+  n
+  
+(0(00))
+  
+---------
+
+((00)(00))
+
+   n
+  / \ -> 2
+  n  n
+  
+--------
+(((LR)(LR))(LR))
+
+0 -> 0
+(00) -> 1
+(0(00)) -> 2
+(((00)(00))(00)) -> 3
+
+      n
+    /  \
+    n   n
+   / \
+  n   n
+  
+((00)((00))(00)) -> 3
+
+Given this representation of the tree, return an Int representing the depth of the tree.
+
+Assumptions:
+- All input is valid
+
+
+
+Psuedocode:
+count=0, max = 0
+each char:
+    if (
+      count++
+      if count > max: max = count
+    if ) count--
+ return max
+ 
+ )(
+        
 */
-// Assume events == 2D array [[1, 159184, 0], [2, 1591.., 0]]
-function totalActiveTime(events) {
-    // O(N) time 
-    // O(1) space
-    let startTime = 0;
-    let totalTime = 0;
-    let numOrders = 0;
-    let currentTime = 0;
-  
-    events.forEach((order) => {
-      numOrders += (order[2] == 0) ? 1 : -1;
-  
-      if (numOrders === 1 && order[2] == 0) {
-        startTime = order[1];
-      }
-  
-      if (numOrders === 0) {
-        currentTime = order[1];
-        totalTime = totalTime + (currentTime - startTime)
-      }
-  
-    });
-      
-    return totalTime;
-  
-  };
-  
-  let events = [
-    [1, 1591846068, 0],
-    [2, 1591846070, 0],
-    [1, 1591846071, 1],
-    [2, 1591846080, 1],
-    [3, 1591846090, 0],
-    [3, 1591846102, 1]
-  ]
-  
-  Event {
-    orderNumber;
-    timestamp;
-    pickup/dropoff;
-  }
-  
-  console.log(totalActiveTime(events), 24);
-  console.log(totalActiveTime([]), 0);
-  
-  
+
+function depth(tree) {
+    let count = 0;
+    let max = 0;
+
+    for (let c of tree) {
+        if (c === "(") {
+            count++;
+            if (count > max) {
+                max = count;
+            }
+        }
+        if (c === ")") {
+            count--;
+        }
+    }
+    return max;
+}
+
+
+console.log(depth('0'), 0);
+console.log(depth('(00)'), 1);
+console.log(depth('(0(00))'), 2);
+console.log(depth('(((00)(00))(00))'), 3);
+
+/*
+checks:
+if tree === '0', return 0
+no more than 2 consecutive 0's
+tree must start with (
+( occurs before 0 or 00
+equal amount of ( to )
+count >= 0 at each step
+*/
+
+/*
+
+100
+0
+50
+
+-----------------
+
+NONZERO -> 1 | 2 | 3 | ... | 9
+DIGIT -> NONZERO | 0
+DIGITS -> DIGIT | DIGIT DIGITS
+INTEGER -> DIGIT | NONZERO DIGITS
+
+BIN_OP -> + | - | * | /
+BIN_EXP -> [INTEGER | BIN_EXP] BIN_OP [INTEGER | BIN_EXP]
+
+-----------------
+
+VAL -> 0 | NODE
+NODE -> ( VAL VAL )
+
+-----------------
+
+5 (10)
+
+*/
+
+
+'((00)0(00))'
+'(0)00()'
+
+'(0)'
+'(0)00()'
+'()'
+
