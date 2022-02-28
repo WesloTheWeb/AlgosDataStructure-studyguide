2a93527a48d03f44e7ac43c449e20fe87e190688
Wesley <wesleyadwebster@gmail.com>
2022-02-28 00:00:58 -0600

    Pure recursion tips

--- Fundamentals/Recursion101.js
+++ Fundamentals/Recursion101.js
@@ -140,3 +140,14 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // 1, 3, 5, 7, 9
 console.log(`Helper Method Recursion Example: ${collectOddValues(array1)}`);
 
 console.log(`#############################`);
+
+/* Pure Recursion Tips
+1. For arrays use methods like slice, the spread operator (...), and concat to make copies of arrays
+so we do not mutate them.
+
+2. Remember that strings are IMMUTABLE, so we will need to use methods like slice, substr, or substring to make copies
+of strings.
+
+3. To make copies of objects, use Object.assign, or the spread operator (...).
+
+*/
