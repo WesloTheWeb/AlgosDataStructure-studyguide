52f42665206c5ecd4f723894f7ded77d306240fa
Wesley Webster <wesleyadwebster@gmail.com>
2022-05-01 23:24:22 -0500

    added cell tower pair learning and updated old one

---
+++ Pair-Learnings/Algo-Marathon-Sessions/pair-cell-tower.js
@@ -0,0 +1,95 @@
+/*#################################################
+DATE: 05/01/2022
+PROBLEM: Cell Tower
+TYPE: Array
+Partner: Gigi T.
+
+Q. You are given an array representing the positions of cell towers along a 2 dimensional line. You are given a second array representing positions of customers across the same 2 dimensional line. Given these two inputs, you must determine a signal strength k such that every customer is covered by at least one cell tower. All cell towers must have the exact same signal strength and covers customers to its left and right equally.
+
+Examples : 
+
+Let's say cell towers are at: [0, 2, 6, 10]
+and customers are at: [0, 5, 11]
+In this case, you only need a cell distance of 1, because customer 5 would be covered by 6 and customer 11 would be covered by 10.
+If the customers were at: [0, 4, 13], you'd need a distance of 3 in order for customer 13 to be covered by 10.
+
+APPROACH
+- find the difference for our listeners to match the cell tower strength
+listener array and the towers array match the element's value
+- two pointer approach
+
+[0, 2, 6, 10] (tower's)
+c1
+[0, 5, 11] (customer's)
+    c2
+c1 - c2
+= 1
+= -1
+
+EDGE CASES
+- tower is empty array null case
+- arrays aren't of equal length?
+
+[0, 2, 6, 10] (tower's)
+   c1
+[0, 5, 11] (listener's)
+   c2
+
+[0, 2, 6, 10] (tower's)       i = 0  0 - 0 = 0 
+       tP                            i = 1 0-5 = -5 
+
+[0, 5, 11] (listener's)  0
+   lp 
+#################################################*/
+
+// function search(listener, towers) {
+
+//   let listenerPointer = 0
+//   let towersPointer = 0
+
+//   for (let i = 0; i < listener.length; i++) {
+//     let signalStrength = towers[towersPointer] - listener[i];
+
+//     // 
+
+//     if (signalStrength === towers[towersPointer]) {
+//       listenerPointer++;
+//     } else if (signalStrength < towers[towersPointer]) {
+//       listenerPointer++;
+//     } else if (signalStrength > towers[towersPointer]) {
+//       towersPointer++;
+//     };
+//   };
+
+//   return signalStrength; 
+// }
+
+/*   n
+[0, 2, 6, 10] (tower's)       
+    t                        
+
+[0, 5, 11] (listener's)  0 tInd = 0 
+    l 
+c
+*/
+
+function search(listener, towers) {
+    let maxTowerDistance = Number.MAX_SAFE_INTEGER;
+    let towerIdx = 0;
+    for (let i = 0; i < listener.length; i++) {
+        while (towerIdx + 1 < towers.length &&
+            Math.abs(listener[i] - towers[towerIdx]) <= Math.abs(listener[i] - towers[towerIdx + 1])) {
+            towerIdx++;
+        }
+        let currentTowerStrength = Math.abs(listener[i] - towers[towerIdx]);
+        maxTowerDistance = Math.min(maxTowerDistance, currentTowerStrength);
+    };
+    return maxTowerDistance;
+};
+
+let tower1 = [0, 2, 6, 10];
+let customer1 = [0, 5, 11]
+let customer2 = [0, 4, 13]
+
+console.log(search(tower1, customer1)); // 1
+console.log(search(tower1, customer2)); // 3
--- Pair-Learnings/Algo-Marathon-Sessions/pair-repeated-elements.js
+++ Pair-Learnings/Algo-Marathon-Sessions/pair-repeated-elements.js
@@ -1,6 +1,11 @@
-/*
-Prompt:
-Given an array and a target count X, return the number of elements that is repeated exactly X times.
+/*#################################################
+DATE: 4/29/2022
+PROBLEM: Number elements repeated X times in array
+TYPE: Array
+Partner: Angel J.
+
+Q. Given an array and a target count X, return the number of elements that is repeated exactly X times.
+
 Function Signature:
 function numElementsRepeatedX(input, target)
 Examples
@@ -21,178 +26,175 @@ Iterate over the hashmap
   count++;
 
   return count
-*/
-// Space: O(n)
-// Time: O(n)
+
+Space: O(n)
+Time: O(n)
+#################################################*/
+
 const numElementsRepeatedX = (input, target) => {
-    let count = 0;
-  
-    const hash = {};
-    input.forEach(number => {
-      if(hash[number]) {
-        hash[number] = hash[number] + 1;
-      } else {
-        hash[number] = 1;
+  let count = 0;
+
+  const hash = {};
+  input.forEach(number => {
+    if (hash[number]) {
+      hash[number] = hash[number] + 1;
+    } else {
+      hash[number] = 1;
+    }
+  });
+  input.forEach(number => hash[number] = (hash[number] || 0) + 1); // another way to do what's above
+
+  for (const key in hash) {
+    if (hash[key] === target) count++;
+  }
+
+  return count;
+}
+
+const formationSolution = (input, target) => {
+
+  let counts = {}
+  let totalCount = 0
+  for (let num of input) {
+    counts[num] = (counts[num] || 0) + 1
+    if (counts[num] == target) {
+      totalCount++
+    } else if (counts[num] == target + 1) {
+      totalCount--
+    }
+  }
+  return totalCount
+}
+
+class Test {
+  constructor(test_name = "", printTests = false) {
+    this.total_count = 0
+    this.problem_count = 0
+    this.total_score = 0
+    this.problem_score = 0
+    this.current_problem = ""
+    this.failed_problems = []
+    console.log(`Beginning Test: ${test_name}`)
+  }
+
+  // Test Helpers
+  test(expected_outcome, outcome, case_num) {
+    if (expected_outcome == outcome) {
+      return this.passed(case_num)
+    }
+    return this.failed(case_num)
+  }
+
+  testMultipleCases(possible_outcomes, outcome, case_num) {
+    var possible_outcome
+    for (possible_outcome of possible_outcomes) {
+      if (this.compareArrays(possible_outcome, outcome)) {
+        return this.passed(case_num)
       }
-    });
-    input.forEach(number => hash[number] = (hash[number] || 0) + 1); // another way to do what's above
-  
-    for(const key in hash) {
-      if(hash[key] === target) count++;
     }
-  
-    return count;
+    return this.failed(case_num)
   }
-  
-  const formationSolution = (input, target) => {
-  
-      let counts = {}
-      let totalCount = 0
-      for (let num of input) {
-        counts[num] = (counts[num] || 0) + 1
-        if (counts[num] == target) {
-          totalCount++
-        } else if (counts[num] == target + 1) {
-          totalCount--
-        }
-      }
-      return totalCount
+
+  testForArrays(expected_outcome, outcome, case_num) {
+    if (this.compareArrays(expected_outcome, outcome)) {
+      return this.passed(case_num)
     }
-    
-    Test Cases:
-    [], 2 => 0
-  
+    return this.failed(case_num)
   }
+
+  compareArrays(array1, array2) {
+    return array1.length === array2.length && array1.filter((a, i) => Array.isArray(a) ? !this.compareArrays(a, array2[i]) : array2[i] !== a).length === 0;
   }
-  class Test {
-    constructor(test_name = "", printTests = false) {
-        this.total_count = 0
-        this.problem_count = 0
-        this.total_score = 0
-        this.problem_score = 0
-        this.current_problem = ""
-        this.failed_problems = []
-        console.log(`Beginning Test: ${test_name}`)
+
+  testMatchAny(expected_outcome, outcome, case_num) {
+    if (this.isEqual(expected_outcome, outcome)) {
+      return this.passed(case_num)
     }
-  
-    // Test Helpers
-    test(expected_outcome, outcome, case_num) {
-        if (expected_outcome == outcome) {
-            return this.passed(case_num)
-        }
-        return this.failed(case_num)
+    return this.failed(case_num)
+  }
+
+  isEqual(array1, array2) {
+    var sortedArr1 = []
+    var sortedArr2 = []
+    for (var a1 of array1) {
+      sortedArr1.push(a1.sort())
     }
-  
-    testMultipleCases(possible_outcomes, outcome, case_num) {
-        var possible_outcome
-        for (possible_outcome of possible_outcomes) {
-            if (this.compareArrays(possible_outcome, outcome)) {
-                return this.passed(case_num)
-            }
-        }
-        return this.failed(case_num)
+    for (var a2 of array2) {
+      sortedArr2.push(a2.sort())
     }
-  
-    testForArrays(expected_outcome, outcome, case_num) {
-        if (this.compareArrays(expected_outcome, outcome)) {
-            return this.passed(case_num)
-        }
-        return this.failed(case_num)
-    }
-  
-    compareArrays(array1, array2) {
-        return array1.length === array2.length && array1.filter((a, i) => Array.isArray(a) ? !this.compareArrays(a, array2[i]) : array2[i] !== a).length === 0;
-    }
-  
-    testMatchAny(expected_outcome, outcome, case_num) {
-        if (this.isEqual(expected_outcome, outcome)) {
-            return this.passed(case_num)
-        }
-        return this.failed(case_num)
-    }
-  
-    isEqual(array1, array2) {
-        var sortedArr1 = []
-        var sortedArr2 = []
-        for (var a1 of array1) {
-            sortedArr1.push(a1.sort())
-        }
-        for (var a2 of array2) {
-            sortedArr2.push(a2.sort())
-        }
-        return this.compareArrays(sortedArr1.sort(), sortedArr2.sort())
-    }  
-  
-    // Test Logistics
-    startProblem(problemName) {
-        this.current_problem = problemName
-        this.problem_score = 0
-        this.problem_count = 0
-        this.failed_problems = []
-    }
-    passed(case_num) {
-        this.total_score += 1
-        this.problem_score += 1
-        this.problem_count += 1
-        this.total_count += 1
-    }
-    failed(case_num) {
-        this.problem_count += 1
-        this.total_count += 1
-        this.failed_problems.push(case_num)
-    }
-    endProblem() {
-        console.log(`\n   ${this.current_problem} Score: ${this.problem_score} / ${this.problem_count}`)
-        if (this.failed_problems.length > 0) {
-            console.log(`   ** Failed Test Cases: ${this.failed_problems}`)
-        }
-    }
-    printFinal() {
-        console.log(`\nTotal Score: ${this.total_score} / ${this.total_count}`)
+    return this.compareArrays(sortedArr1.sort(), sortedArr2.sort())
+  }
+
+  // Test Logistics
+  startProblem(problemName) {
+    this.current_problem = problemName
+    this.problem_score = 0
+    this.problem_count = 0
+    this.failed_problems = []
+  }
+  passed(case_num) {
+    this.total_score += 1
+    this.problem_score += 1
+    this.problem_count += 1
+    this.total_count += 1
+  }
+  failed(case_num) {
+    this.problem_count += 1
+    this.total_count += 1
+    this.failed_problems.push(case_num)
+  }
+  endProblem() {
+    console.log(`\n   ${this.current_problem} Score: ${this.problem_score} / ${this.problem_count}`)
+    if (this.failed_problems.length > 0) {
+      console.log(`   ** Failed Test Cases: ${this.failed_problems}`)
     }
   }
-        
-  var test = new Test("")
-  
-  class ListNode {
-    constructor(value = 0, next = null) {
-        this.value = value
-        this.next = next
-    }
+  printFinal() {
+    console.log(`\nTotal Score: ${this.total_score} / ${this.total_count}`)
   }
-  
-  function arrayify(head) {
-    var ptr = head
-    var array = []
-    while (ptr != null) {
-        array.push(ptr.value)
-        ptr = ptr.next
-    }
-    return array
+}
+
+var test = new Test("")
+
+class ListNode {
+  constructor(value = 0, next = null) {
+    this.value = value
+    this.next = next
   }
-  
-  class TreeNode {
-    constructor(value = 0, leftChild = null, rightChild = null) {
-        this.value = value
-        this.left = leftChild
-        this.right = rightChild
-    }
+}
+
+function arrayify(head) {
+  var ptr = head
+  var array = []
+  while (ptr != null) {
+    array.push(ptr.value)
+    ptr = ptr.next
   }
-  
-  function arrayifyTree(root) {
-    if (!root) { return [] }
-    var queue = []
-    var array = []
-    queue.push(root)
-    while (queue.length !== 0) {
-        var node = queue.shift()
-        array.push(node.value)
-        if (node.left) { queue.push(node.left) }
-        if (node.right) { queue.push(node.right) }
-    }
-    return array
+  return array
+}
+
+class TreeNode {
+  constructor(value = 0, leftChild = null, rightChild = null) {
+    this.value = value
+    this.left = leftChild
+    this.right = rightChild
   }
-  
-  console.log(numElementsRepeatedX([1, 2, 3, 1, 2, 3], 2), 3);
-  console.log(numElementsRepeatedX([1, 2, 3, 1, 2, 3], 3), 0);
-  console.log(numElementsRepeatedX([1, 3, 3, 5, 5, 5, 5, 5, 3], 3), 1);
+}
+
+function arrayifyTree(root) {
+  if (!root) { return [] }
+  var queue = []
+  var array = []
+  queue.push(root)
+  while (queue.length !== 0) {
+    var node = queue.shift()
+    array.push(node.value)
+    if (node.left) { queue.push(node.left) }
+    if (node.right) { queue.push(node.right) }
+  }
+  return array
+}
+
+console.log(numElementsRepeatedX([1, 2, 3, 1, 2, 3], 2), 3);
+console.log(numElementsRepeatedX([1, 2, 3, 1, 2, 3], 3), 0);
+console.log(numElementsRepeatedX([1, 3, 3, 5, 5, 5, 5, 5, 3], 3), 1);
