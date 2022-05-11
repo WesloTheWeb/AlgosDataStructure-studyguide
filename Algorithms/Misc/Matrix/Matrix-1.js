e356ecc6957fa2149c5892fe9e9fc0b07b337ec3
Wesley <wesleyadwebster@gmail.com>
2022-05-10 22:35:54 -0500

    matrix problem with Jordan assistant. Good fundamental

--- Algorithms/Misc/Matrix/Matrix-1.js
+++ Algorithms/Misc/Matrix/Matrix-1.js
@@ -1,20 +1,22 @@
-/*#######################################################################################################
+/*###############################################################################################
 Q. Given a matrix of integers, determine if the matrix is monotonically increasing along all rows and all columns.
 This means that along every row (left to right) and column (top to bottom),
 each successive value is AT LEAST as large as the previous value. For example:
 
-1 1 1 2
-1 2 3 3
-3 4 5 6
-satisfies the requirements.
+1   1   1   2
 
+1   2   3   3
+
+3   4   5   6
+
+satisfies the requirements.
 You may assume that every row is the same length, ie, the matrix will be rectangular.
 
 APPROACH:
 - We need to account for left & right and top & bottom.
-- Could use something along the line of [x1, y1], and [x2, y2].
-- it can account for more than 2 elements i.e [a1, b1, c1, d1, e1] = [a2, b2, c2, d2, e2]
-- If x1 < x2 then it is NOT monotonalically increasing
+- We are able to do this by going right, bottom.
+- If conditions are not meant we return false
+- Account for the edge cases of undefined.
 
 PSEUDO CODE
 - iterate horizontally and vertically (double for loops)
@@ -23,33 +25,33 @@ PSEUDO CODE
 - If it has the same value, continue going,
 - If next element is greater than current element, it IS monotonically increasing.
 
-
 NOTE:
-There aren't that many patterns. Basically you should be able to iterate through a 2d array in a row-major way or a column-major way. More advanced would be moving through the matrix along other orderings (spiral, diagonals, etc).
-
-7:53 PM
+There aren't that many patterns. Basically you should be able to iterate through a 2d array in a row-major way or a column-major way. 
+More advanced would be moving through the matrix along other orderings (spiral, diagonals, etc).
 But at the end of the day, it's all about:
 
 Keeping track of two indices (row and column) in an organized matter.
 Deciding how to move from one location to the next.
 Dealing with the edges.
-Deciding what work needs to be done at each visited location.
-
-
+Deciding what work needs to be done at each visited location.             
 
 #######################################################################################################*/
 
 function solution(matrix) {
-    for (let col = 0; col < matrix.length; col++) {
-        if (matrix[col] < matrix[col + 1]) {
-            return false;
-        } else {
-            true;
-        }
-        for (let row = 0; row < matrix.length; row++) {
-
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix.length; col++) {
+            // Check the right cell
+            if (matrix[row][col + 1] !== undefined && matrix[row][col] > matrix[row][col + 1]) {
+                return false;
+            };
+            // Check the bottom cell
+            // matrix[row] && matrix[row][col] && ....
+            if (matrix[row + 1] !== undefined && matrix[row][col] > matrix[row + 1][col]) {
+                return false;
+            };
         };
     };
+    return true;
 };
 
 // Test Cases
@@ -57,10 +59,7 @@ const matrix1 = [
     [1, 2],
     [3, 4]
 ]; // true
-/*
-  1  2
-  3  4
-*/
+
 const matrix2 = [
     [1, 1, 1, 2],
     [1, 2, 3, 3],
@@ -81,8 +80,8 @@ const matrix5 = [
     [3, 4]
 ]; // false
 
-solution(matrix1); // true
-solution(matrix2); // true
-solution(matrix3); // false
-solution(matrix4); // false
-solution(matrix5); // false
+console.log(solution(matrix1)); // true
+console.log(solution(matrix2)); // true
+console.log(solution(matrix3)); // false
+console.log(solution(matrix4)); // false
+console.log(solution(matrix5)); // false
