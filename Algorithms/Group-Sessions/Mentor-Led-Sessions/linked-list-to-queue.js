44d4153339de0bd55102e4226e7aedc666871bb2
Wesley <wesleyadwebster@gmail.com>
2022-07-03 00:25:57 -0500

    cleaned up linked list to queue and formatted correctly

--- Algorithms/Group-Sessions/Mentor-Led-Sessions/linked-list-to-queue.js
+++ Algorithms/Group-Sessions/Mentor-Led-Sessions/linked-list-to-queue.js
@@ -1,6 +1,11 @@
-/*
+/*########################
+TYPE: Linked List & Queue
+Question:
+Implement a basic queue class using a linked list as the underlying storage. Queues have two critical methods, 
+enqueue() and dequeue() to add and remove an item from the queue, respectively. You'll also need a constructor for 
+your class, and for convenience, add a size() method that returns the current size of the queue. 
 
-Implement a basic queue class using a linked list as the underlying storage. Queues have two critical methods, enqueue() and dequeue() to add and remove an item from the queue, respectively. You'll also need a constructor for your class, and for convenience, add a size() method that returns the current size of the queue. All of these methods should run in O(1) time!
+All of these methods should run in O(1) time!
 
 Examples:
 
@@ -13,6 +18,9 @@ console.log(q.dequeue()); // 2
 console.log(q.size()) // 1
 console.log(q.dequeue()); // 3
 
+
+Approach:
+
 (back) H - n - n - n - T (front)
 
 represent new item as T
@@ -24,27 +32,31 @@ remove an item: iterate through list until you grab the last, remove it and assi
 Queue: new items come in through the front, items can only be removed from the back
 FIRST IN, FIRST OUT, LAST IN LAST OUT
 
-*/
+Time Complexity:
+Space Complexity:
 
+Pseudo Code:
+
+########################*/
 
 class ListNode {
     constructor(value = 0, next = null, prev = null) {
-        this.value = value
-        this.next = next
-        this.prev = prev
-    }
-}
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    };
+};
 
 class LLQueue {
     constructor() {
-        this.head = null
-        this.tail = null
-        this.size = 0
-    }
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    };
 
     length() {
         return this.size;
-    }
+    };
 
     enqueue(value) {
         let node = new ListNode(value);
@@ -58,8 +70,7 @@ class LLQueue {
             this.tail = node;
         }
         this.size += 1;
-
-    }
+    };
 
     dequeue() {
         let nodeToRemove;
@@ -78,9 +89,10 @@ class LLQueue {
 
         this.size -= 1;
         return nodeToRemove.value;
-    }
-}
+    };
+};
 
+// Test Cases:
 const q = new LLQueue();
 console.log(q.length()) // 0
 q.enqueue(2);
