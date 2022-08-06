a4863b30cd02f30c94949807686174ecd7af9c73
Wesley <wesleyadwebster@gmail.com>
2022-08-05 23:03:55 -0500

    update of linked list problems and scrubbing of the pads

--- Golden/LL-2.js
+++ Golden/LL-2.js
@@ -1,3 +1,23 @@
-function solution (l1, l2) {
+function solution(l1, l2) {
 
 };
+
+const swapPairs = (head) => {
+    // create dummy node to deal with null head
+    const dummy = new Node();
+    dummy.next = head; // makes the next pointer point to head
+
+    let temp = dummy;
+
+    while (temp.next && temp.next.next) { // while there is a next val and next next val
+        let node1 = temp.next;
+        let node2 = temp.next.next;
+
+        temp.next = node2;
+        node1.next = node2.next;
+        node2.next = node1;
+        temp = node1;
+    };
+
+    return dummy.next;
+}; 
