32e6f4dc763f1cd47c39d2a8b1958ac210042fd1
Wesley <wesleyadwebster@gmail.com>
2022-12-31 13:10:17 -0600

    minor edit

--- Algo-Learning-Resources/Practice/template.js
+++ Algo-Learning-Resources/Practice/template.js
@@ -22,31 +22,31 @@ HELPER FUNCTIONS
 
 // Linked Lists:
 class Node {
-    constructor(val, next=null) {
-      this.val = val;
-      this.next = next;
-    };
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
   };
-  
-  function toString(head) {
-    if (!head)
-      return "<empty>";
-  
-    let parts = [];
-    while(head) {
-      parts.push(head.val);
-      head = head.next;
-    };
-  
-    return parts.join(" -> ");
+};
+
+function toString(head) {
+  if (!head)
+    return "<empty>";
+
+  let parts = [];
+  while (head) {
+    parts.push(head.val);
+    head = head.next;
   };
+
+  return parts.join(" -> ");
+};
 
 // Binary Trees  
 class TreeNode {
   constructor(val, left = null, right = null) {
-      this.val = val;
-      this.left = left;
-      this.right = right;
+    this.val = val;
+    this.left = left;
+    this.right = right;
   };
 };
 
