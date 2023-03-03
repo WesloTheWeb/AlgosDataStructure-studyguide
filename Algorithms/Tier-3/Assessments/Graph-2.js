5cf57a4b008f61c211d0e3514cd49a40d23da357
Wesley <wesleyadwebster@gmail.com>
2023-03-02 19:09:39 -0600

    moved stuff

---
+++ Algorithms/Tier-3/Assessments/Graph-2.js
@@ -0,0 +1,28 @@
+/*
+Question:
+Suppose you are given an edge list of all the members of a social network. This edge list contains tuples of the format.
+
+(friend1, friend2)
+
+All users of this social network exist in at least one tuple of this edge list. All users have a unique name.
+
+Return a boolean of whether this social network is connected or not.
+
+Definition
+A social network is connected when all members share a path of friends with each other.
+
+User1--User2
+ |     |
+User3--User4---User5
+ |_______________|
+A social netowrk is disconnected when there is at least one group of friends who are completely disconnected from the rest of the network
+
+User1--User2
+ |            
+User3  User4---User5
+
+*/
+
+function solution(edge_list) {
+
+}
